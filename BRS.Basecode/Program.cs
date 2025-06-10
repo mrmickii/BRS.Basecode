e33825b34a724342e061ef4e4ad07613929b3ea6
Carlo Garcia <garciaram.cal@gmail.com>
2025-06-10 15:20:47 +0800

    Revert "Created API for user"
    This reverts commit 92801cf356a35e6be8c23a4a6b5dfcc26598c1ac.

--- BRS.Basecode/Program.cs
+++ BRS.Basecode/Program.cs
@@ -1,10 +1,3 @@
-using BRS.Basecode.Data;
-using BRS.Basecode.Data.Interfaces;
-using BRS.Basecode.Data.Repositories;
-using BRS.Basecode.Services.Interfaces;
-using BRS.Basecode.Services.Services;
-using Microsoft.EntityFrameworkCore;
-
 namespace BRS.Basecode
 {
     public class Program
@@ -14,27 +7,11 @@ namespace BRS.Basecode
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-            // Register Repositories and Services
-            builder.Services.AddScoped<IUserRepository, UserRepository>();
-            builder.Services.AddScoped<IUserService, UserService>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
-            builder.Services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(policy =>
-                {
-                    policy.AllowAnyOrigin()
-                          .AllowAnyHeader()
-                          .AllowAnyMethod();
-                });
-            });
 
             var app = builder.Build();
 
@@ -45,11 +22,13 @@ namespace BRS.Basecode
                 app.UseSwaggerUI();
             }
 
-            app.UseRouting();
-            app.UseCors();
             app.UseHttpsRedirection();
+
             app.UseAuthorization();
+
+
             app.MapControllers();
+
             app.Run();
         }
     }
